feae6a72983439bb8b3775b6e409bc0b3a339ae2
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-02-18 14:39:15 +0200

    task 11 commit, ya maw

---
+++ task11/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell\desktop\development\django_blog\task_11\task11\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
